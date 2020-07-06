6ea7a480b32a15fcc9f36c0f1f5f20f2ed6be1bc
Ashutosh Malviya <ashutoshmalviya1000@gmail.com>
2020-07-06 13:29:23 +0530

    improved notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
   return (
     <div className="note">
-      <h1>demo note</h1>
-      <p>let's relearn passport.js</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
