69a755791cef82cc1fd5832922b12eaf0e50e45d
Ashutosh Malviya <ashutoshmalviya1000@gmail.com>
2020-07-08 01:30:26 +0530

    updated stylesheet and minor refactoring

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import {v4 as uuidv4} from "uuid";
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { Zoom } from '@material-ui/core';
 
 function CreateArea(props) {
   const [newNote, setNewNote] = useState({
@@ -16,34 +19,46 @@ function CreateArea(props) {
       };
     });
   }
+  function submitNote(event) {
 
+    props.onAdd(newNote);
+    setNewNote({
+      key: uuidv4(),
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  //rendering title and button when textarea is clicked
+  const [isExpanded,setIsExpanded] = useState(false);
+  function handleClick() {
+    setIsExpanded(true);
+  }
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-            e.preventDefault();
-          props.onAdd(newNote);
-          setNewNote({
-            key: uuidv4(),
-            title: "",
-            content: "",
-          });
-        }}
+      <form className="create-note"
+        onSubmit={submitNote}
       >
         <input
           onChange={handleChange}
           name="title"
           placeholder="Title"
          value={newNote.title}
+         style={isExpanded?null:{visibility:"hidden",position:"absolute"}}
         />
         <textarea
+          onClick={handleClick}
           onChange={handleChange}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded?"3":"1"}
           value={newNote.content}
         />
-        <button>Add</button>
+        <Zoom in={isExpanded}>
+        <Fab><AddIcon/></Fab>
+        </Zoom>
+        
       </form>
     </div>
   );
